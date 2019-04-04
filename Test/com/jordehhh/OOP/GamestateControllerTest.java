c1727d694947c46e2648fdd32114bc05416e3274
jd <jimdijkxhoorn@gmail.com>
2019-04-04 12:21:04 +0200

    MURICA

---
+++ Test/com/jordehhh/OOP/GamestateControllerTest.java
@@ -0,0 +1,51 @@
+package com.jordehhh.OOP;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GamestateControllerTest {
+
+    @Test
+    public void testMoveLeft() throws Exception {
+        /*GamestateController gamestateController = new GamestateController(10, 10);
+        gamestateController.player.getHuidigeLocatieY()
+        assertTrue(gamestateController.moveLeft());*/
+    }
+
+    @Test
+    public void gotoLeft() {
+    }
+
+    @Test
+    public void moveRight() {
+    }
+
+    @Test
+    public void gotoRight() {
+    }
+
+    @Test
+    public void moveUp() {
+    }
+
+    @Test
+    public void goUp() {
+    }
+
+    @Test
+    public void moveDown() {
+    }
+
+    @Test
+    public void goDown() {
+    }
+
+    @Test
+    public void isMoveAllowed() {
+    }
+
+    @Test
+    public void setMoveAllowed() {
+    }
+}
