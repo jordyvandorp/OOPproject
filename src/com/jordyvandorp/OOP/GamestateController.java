a8a5e46174cce449b0e3e9c725588e4741191ab0
jordy <jordydorp@gmail.com>
2019-04-04 14:35:54 +0200

    removed unnecessary draw()

--- src/com/jordyvandorp/OOP/GamestateController.java
+++ src/com/jordyvandorp/OOP/GamestateController.java
@@ -27,21 +27,9 @@ public class GamestateController extends JPanel implements KeyListener, ActionLi
         blockWidth = 400/col;
         blockHeight = 400/row;
     }
-    public void draw(Graphics2D g) {
-        for (int i = 0; i < map.length; i++) {
-            for (int j = 0; j < map[0].length; j++) {
-                if (map[i][j] > 0) {
-                    g.setColor(Color.white);
-                    g.fillRect(j * blockWidth + 80, i * blockHeight + 50, blockWidth, blockHeight);
 
-                    g.setStroke(new BasicStroke(3));
-                    g.setColor(Color.black);
-                    g.drawRect(j * blockWidth + 80, i * blockHeight + 50, blockWidth, blockHeight);
 
-                }
-            }
-        }
-    }
+
 
 
     /*
