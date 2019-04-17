51c3ad0cc21d2e88d9084ffa735faaa0fe7d0b67
jordy <jordydorp@gmail.com>
2019-04-17 15:20:37 +0200

    edited exit to make window clearer

--- src/com/jordyvandorp/OOP/Exit.java
+++ src/com/jordyvandorp/OOP/Exit.java
@@ -4,21 +4,24 @@ import javax.swing.*;
 
 public class Exit {
 
+    //private Background bg = new Background();
     public Exit(){}
 
     public void exitReached(){
         JFrame frame = new JFrame();
-        final int frameWidth = 300;
-        final int frameHeight = 80;
+        final int frameWidth = 500;
+        final int frameHeight = 100;
         frame.setSize(frameWidth, frameHeight);
         frame.setResizable(false);
         JPanel panel = new JPanel();
         JLabel label = new JLabel("Congratulations, you won!");
+        label.setFont(new java.awt.Font("Tahoma", 0, 36));
         panel.add(label);
         frame.add(panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Victory");
+        frame.setTitle("You've won!");
         frame.setVisible(true);
+
 
     }
 }
